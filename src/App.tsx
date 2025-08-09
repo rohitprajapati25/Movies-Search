b19ad1e0d1b415dd5a25c2f69ec07724b7b6092e
Rohit Prajapati <pr4901958@gmail.com>
2025-08-09 15:10:50 +0530

    Update App.tsx
    Add Download Button....

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,3 @@
-// import "./styles.css";
-// import React, { useState } from "react";
-
-// export default function App() {
-//   const [movies, setMovies] = useState([]);
-//   const [query, setQuery] = useState("");
-
-//   const searchMovies = async () => {
-//     try {
-//       const res = await fetch(
-//         `https://www.omdbapi.com/?s=${query}&apikey=e76f4147`
-//       );
-//       const data = await res.json();
-
-//       setMovies(data.Search);
-//     } catch (err) {
-//       setMovies([]);
-//     }
-//   };
-
-//   return (
-//     <>
-//       <div className="p-10">
-//         <div className="flex">
-//           <i className="ri-movie-fill text-7xl"></i>
-//           <h1 className=" text-orange-500 text-6xl w-auto">Movies Search</h1>
-//         </div>
-//         <input
-//           type="text"
-//           className="w-6/12 mt-10"
-//           placeholder="Search movies hear..."
-//           onChange={(e) => {
-//             setQuery(e.target.value);
-//           }}
-//           value={query}
-//         />
-//         <button
-//           className="border p-2 bg-cyan-700 text-white min-w-12"
-//           onClick={searchMovies}
-//         >
-//           Search
-//         </button>
-//       </div>
-//       {movies.map((movie) => {
-//         return (
-//           <div>
-//             <div className="gap-10 h-1/2 w-1/2 m-10 border border-gray-500 p-10 bg-blue-100">
-//               <img src={movie.Poster} alt="" />
-//               <h1 className="text-[25px] font-semibold">{movie.Title}</h1>
-//               <h4>Year : {movie.Year}</h4>
-//               <h4>Type : {movie.Type}</h4>
-//             </div>
-//           </div>
-//         );
-//       })}
-//     </>
-//   );
-// }
-
-
 import "./styles.css";
 import React, { useState } from "react";
 
@@ -124,6 +64,8 @@ export default function App() {
                   <p className="text-gray-600">📅 Year: {movie.Year}</p>
                   <p className="text-gray-600 capitalize">🎬 Type: {movie.Type}</p>
                 </div>
+               <a href={`https://bollyflix.fish/?s=${movie.Title}`} target="_blank"><button className="border border-1 rounded h-8 w-50 hover:bg-black  hover:text-white">Click To Download</button></a> 
+            
               </div>
             ))}
           </div>
